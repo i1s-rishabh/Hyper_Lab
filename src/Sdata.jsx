dc09a6bb60809453d2fe7a59d5074fc0801f390d
Rishabh Sharma <sharmarishabh.2018@gmail.com>
2021-06-10 10:43:31 +0530

    Hyper_Lab

---
+++ src/Sdata.jsx
@@ -0,0 +1,40 @@
+import app from "../src/Images/s1.svg";
+import web from "../src/Images/s2.svg";
+import data from "../src/Images/s3.svg";
+import security from "../src/Images/s4.svg";
+import cloud from "../src/Images/s5.svg";
+import server from "../src/Images/s6.svg";
+
+const Sdata = [
+    {
+        imgsrc: app,
+        title: "App Development",
+    },
+
+    {
+        imgsrc: web,
+        title: "Web Development",
+    },
+
+    {
+        imgsrc: data,
+        title: "Data Management",
+    },
+
+    {
+        imgsrc: security,
+        title: "Network Security",
+    },
+
+    {
+        imgsrc: cloud,
+        title: "Cloud Computing",
+    },
+
+    {
+        imgsrc: server,
+        title: "Server Security",
+    },
+];
+
+export default Sdata;
